bc09c20370ba61643b8ab665c75e0772b041b3d1
2Kelvin <kvin.mwangi254@gmail.com>
2022-07-29 17:48:11 +0300

    added a Row layout

--- app/src/main/java/com/example/composetutorial101/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial101/MainActivity.kt
@@ -3,15 +3,20 @@ package com.example.composetutorial101
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.composetutorial101.ui.theme.ComposeTutorial101Theme
 
@@ -30,9 +35,22 @@ data class Message(val sender: String, val body: String)
 // Composable functions can only be called from other Composable functions
 @Composable
 fun MessageCard(msg: Message) {
-    Column() {
-        Text(text = msg.body)
-        Text(text = msg.sender, color = Color.LightGray, fontSize = 13.sp)
+    Row(Modifier.padding(8.dp)) {
+        Image(
+            painter = painterResource(R.drawable.profile_picture),
+            contentDescription = "Sender's picture",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column() {
+            Text(text = msg.body)
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = msg.sender, color = Color.Gray, fontSize = 13.sp)
+        }
     }
 }
 
