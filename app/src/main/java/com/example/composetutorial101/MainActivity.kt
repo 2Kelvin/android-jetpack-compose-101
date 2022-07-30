914c483071985e155d2c206ed32c88b36629f148
2Kelvin <kvin.mwangi254@gmail.com>
2022-07-30 06:27:01 +0300

    added Material design

--- app/src/main/java/com/example/composetutorial101/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial101/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
@@ -24,10 +25,18 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent { // setContent block defines the activity's layout where composable functions are called
-            MessageCard(msg = Message("Jolene", "Your beauty is beyond compare"))
+            ComposeTutorial101Theme { // Material theme installed by default by JetPack Compose
+                MessageCard(msg = Message("Jolene", "Your beauty is beyond compare"))
+            }
         }
     }
 }
+// using a Material Theme allows composables to inherit styles as defined in the app's theme ensuring consistency across the app
+// the theme is called in 'setContent' above and in 'Preview' Composable function below.
+// the Empty Compose Activity template generates a default theme for your project that allows you to customize MaterialTheme
+// you can find your custom theme in the Theme.kt file in the ui.theme subpackage.
+// Material Design is built around three pillars: 'Color', 'Typography' and 'Shape'
+
 
 // this is a message object
 data class Message(val sender: String, val body: String)
@@ -42,14 +51,30 @@ fun MessageCard(msg: Message) {
             modifier = Modifier
                 .size(40.dp)
                 .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
         )
 
         Spacer(modifier = Modifier.width(8.dp))
 
         Column() {
-            Text(text = msg.body)
+            Surface( // Surface composable allows customizing of the message body's shape and elevation
+                shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp
+            ) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(4.dp),
+                    style = MaterialTheme.typography.body2
+                )
+            }
+
             Spacer(modifier = Modifier.height(4.dp))
-            Text(text = msg.sender, color = Color.Gray, fontSize = 13.sp)
+
+            Text(
+                text = msg.sender,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+            )
         }
     }
 }
@@ -58,6 +83,8 @@ fun MessageCard(msg: Message) {
 @Preview(showBackground = true)
 @Composable
 fun MessageCardPreview() {
-    MessageCard(msg = Message("Jolene", "Your beauty is beyond compare."))
-    //MessageCard(msg = Message("Android Devs", "Jetpack Compose is so cool."))
+   ComposeTutorial101Theme {
+       MessageCard(msg = Message("Jolene", "Your beauty is beyond compare."))
+       //MessageCard(msg = Message("Android Devs", "Jetpack Compose is so cool."))
+   }
 }
