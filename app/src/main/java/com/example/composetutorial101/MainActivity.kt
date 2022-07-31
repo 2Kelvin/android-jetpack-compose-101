af2e67e56c4929058cadc6aea3112c070e810e5c
2Kelvin <kvin.mwangi254@gmail.com>
2022-08-01 01:13:04 +0300

    added animations and more messages

--- app/src/main/java/com/example/composetutorial101/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial101/MainActivity.kt
@@ -4,14 +4,19 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
@@ -24,7 +29,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent { // setContent block defines the activity's layout where composable functions are called
             ComposeTutorial101Theme { // Material theme installed by default by JetPack Compose
-                MessageCard(msg = Message("Jolene", "Your beauty is beyond compare"))
+                Conversation(messages = SampleData.conversationSample)
             }
         }
     }
@@ -54,7 +59,18 @@ fun MessageCard(msg: Message) {
 
         Spacer(modifier = Modifier.width(8.dp))
 
-        Column {
+        // Composable functions can store local state in memory by using 'remember'
+        // and track changes to the value passed to 'mutableStateOf'
+        // when the value is updated, the Composables & their children get redrawn automatically
+        // any changes to the state automatically updates the UI.
+        // keeping track if the message is expanded or not
+        var isExpanded by remember { mutableStateOf(false) }
+        // surfaceColor will be updated gradually from one color to the other
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface
+        )
+        // toggling isExpanded variable when we click on this Column
+        Column(Modifier.clickable { isExpanded = !isExpanded }){
             Text(
                 text = msg.sender,
                 color = MaterialTheme.colors.secondaryVariant,
@@ -65,14 +81,32 @@ fun MessageCard(msg: Message) {
 
             Surface( // Surface composable allows customizing of the message body's shape and elevation
                 shape = MaterialTheme.shapes.medium,
-                elevation = 1.dp
+                elevation = 1.dp,
+                // surfaceColor color will be changing gradually from primary to surface
+                color = surfaceColor,
+                // animateContentSize will change the Surface size gradually
+                modifier = Modifier
+                    .animateContentSize()
+                    .padding(1.dp)
             ) {
                 Text(
                     text = msg.body,
                     modifier = Modifier.padding(4.dp),
-                    style = MaterialTheme.typography.body2
+                    style = MaterialTheme.typography.body2,
+                    // If the message is expanded, we display all its content
+                    // otherwise we only display the first line
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1
                 )
             }
+        }
+    }
+}
+
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn { // renders only the elements visible on screen,it's designed to be very efficient for long lists
+        items(messages) { text ->
+            MessageCard(msg = text)
         }
     }
 }
@@ -89,9 +123,8 @@ fun MessageCard(msg: Message) {
 // the '@Preview' annotation is used on a composable function that does not take in parameters
 @Preview(showBackground = true)
 @Composable
-fun MessageCardPreview() {
+fun PreviewConversation() {
    ComposeTutorial101Theme {
-       MessageCard(msg = Message("Jolene", "Your beauty is beyond compare."))
-       //MessageCard(msg = Message("Android Devs", "Jetpack Compose is so cool."))
+       Conversation(messages = SampleData.conversationSample)
    }
 }
